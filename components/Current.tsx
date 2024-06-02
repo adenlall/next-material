e18f831411562f981a3764c1822ead4e0799be65
adenlall <adenlall@skiff.com>
2024-06-02 21:00:41 +0100

    updae current work

--- components/Current.tsx
+++ components/Current.tsx
@@ -4,7 +4,9 @@ export default function Current() {
             <div className="w-3 h-3 absolute top-5 right-5 primary-container animate-ping"></div>
             <h2 className="m-0">Cuurently Working On</h2>
             <p className="m-0 pl-4 ">
-                <a href="https://github.com/adenlall/travelo"><strong className="underline">Travelo</strong> - Ongoing developement</a>
+                <a href="https://github.com/adenlall/travelo"><strong className="underline mr-1">Travelo</strong>
+                {" - Ongoing developement"}
+                </a>
             </p>
             <p className="m-0 pl-4">
             Travelo is a platform that connects travelers with potential trip companions and local guides. It allows users to find and connect with other like-minded individuals to share travel experiences, explore destinations together, and gain insider knowledge from local experts.
